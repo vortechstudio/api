6bcc3079df98a2e69a154bc370ae0c6eb9ac9da4
Vortechstudio <contact@vortechstudio.fr>
2024-02-07 21:18:52 +0100

    feat(account): ajout de la fonctionnalité de mise à jour du profil
    - Ajout de la route /profil/update
    - Création des fichiers update_mail.blade.php, UpdateController.php et UpdateMailNotification.php
    
    BREAKING CHANGE: ajout de la fonctionnalité de mise à jour du profil
    
    refs: #13

--- routes/api/account.php
+++ routes/api/account.php
@@ -3,4 +3,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('account')->as('account')->group(function () {
     Route::get('/profil', \App\Http\Controllers\Account\ProfilController::class);
+    Route::put('/profil/update');
 });
